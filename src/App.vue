46dccf61be83c9923beb4963732c67c7e4f933c9
Jacob Champlin <jacob.a.champlin@gmail.com>
2019-02-24 18:36:13 -0700

    vue counter component and basic structure

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <Counter msg="Counter Value" multiply=1></Counter>
+    <Counter msg="Counter Value" multiply=5></Counter>
+    <Counter msg="Counter Value" multiply=10></Counter>
+  </div>
+</template>
+
+<script>
+import Counter from './components/Counter.vue';
+
+export default {
+  name: 'app',
+  components: {
+    Counter
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/Counter.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="counter">
+    <h1>{{ msg }} - ({{ num }} * {{ multiply }}) = {{ total }}</h1>
+    <div>
+      <button v-on:click="clickCounter">{{ btnText }}</button>
+      <button v-on:click="calculate">calculate</button>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'Counter',
+    props: {
+      msg: String,
+      multiply: String
+    },
+    data: function() {
+      return {
+        num: 0,
+        total: 0,
+        btnText: 'Click Counter',
+      }
+    },
+    methods: {
+      clickCounter: function() {
+        alert(`Clicked ${this.num} times`)
+      },
+      calculate: function() {
+        this.num++;
+        this.total = this.num * this.multiply;
+      },
+    },
+  }
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+button {
+  border: solid .1rem #41B883;
+  border-radius: .1rem;
+  color: #41B883;
+  background-color: #ffffff;
+  margin: .3rem;
+  padding: .3rem;
+}
+
+.counter {
+  color: #41B883;
+}
+</style>
